642a6494fc114f9f4c379d1d51c3029cb1edd956
gareth <gareth.stokes@paidright.io>
2026-02-22 10:15:06 +1100

    feat(github): add fetchCommitDiff for retrieving commit diffs

---
+++ wisp-srv/src/Infra/GitHub/Commits.hs
@@ -0,0 +1,69 @@
+module Infra.GitHub.Commits
+  ( fetchCommitDiff
+  , CommitWithDiff(..)
+  ) where
+
+import Data.Aeson (ToJSON(..), FromJSON(..), object, (.=), (.:), withObject)
+import Data.Text (Text)
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as TE
+import qualified Data.ByteString.Lazy as LBS
+import Network.HTTP.Client
+import Network.HTTP.Client.TLS (tlsManagerSettings)
+import Network.HTTP.Types.Status (statusCode)
+
+-- | A commit with its diff content
+data CommitWithDiff = CommitWithDiff
+  { cwdSha :: Text
+  , cwdMessage :: Text
+  , cwdAuthor :: Text
+  , cwdUrl :: Text
+  , cwdDiff :: Maybe Text
+  , cwdDiffError :: Maybe Text
+  } deriving (Show, Eq)
+
+instance ToJSON CommitWithDiff where
+  toJSON c = object
+    [ "sha" .= cwdSha c
+    , "message" .= cwdMessage c
+    , "author" .= cwdAuthor c
+    , "url" .= cwdUrl c
+    , "diff" .= cwdDiff c
+    , "diff_error" .= cwdDiffError c
+    ]
+
+instance FromJSON CommitWithDiff where
+  parseJSON = withObject "CommitWithDiff" $ \v -> CommitWithDiff
+    <$> v .: "sha"
+    <*> v .: "message"
+    <*> v .: "author"
+    <*> v .: "url"
+    <*> v .: "diff"
+    <*> v .: "diff_error"
+
+-- | Fetch the diff for a single commit
+-- Returns the raw diff text or an error message
+fetchCommitDiff
+  :: Text  -- ^ Repository owner
+  -> Text  -- ^ Repository name
+  -> Text  -- ^ Commit SHA
+  -> Text  -- ^ Access token
+  -> IO (Either Text Text)
+fetchCommitDiff owner repo sha accessToken = do
+  manager <- newManager tlsManagerSettings
+  let url = "https://api.github.com/repos/" <> T.unpack owner <> "/" <> T.unpack repo <> "/commits/" <> T.unpack sha
+  req <- parseRequest url
+  let headers =
+        [ ("Authorization", "Bearer " <> TE.encodeUtf8 accessToken)
+        , ("User-Agent", "wisp-srv")
+        , ("Accept", "application/vnd.github.diff")
+        ]
+  let authReq = req { requestHeaders = headers }
+  response <- httpLbs authReq manager
+
+  let status = statusCode (responseStatus response)
+  case status of
+    200 -> pure $ Right $ TE.decodeUtf8 $ LBS.toStrict $ responseBody response
+    404 -> pure $ Left "404: Commit not found (may have been force-pushed)"
+    403 -> pure $ Left "403: Rate limited or access denied"
+    _ -> pure $ Left $ "GitHub API error: " <> T.pack (show status)
